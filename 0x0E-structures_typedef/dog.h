21e7c3695eefe48245b9a69c6d5145b383314b48
HamzaDXTR101 <hamzanetro47@gmail.com>
2023-04-17 17:38:25 -0700

    update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -3,9 +3,9 @@
 
 /**
  * struct dog - Dog attributes
- * name: The name of the dog.
- * age: The age of the dog.
- * owner: The owner of the dog.
+ * @name : The name of the dog.
+ * @age : The age of the dog.
+ * @owner : The owner of the dog.
  *
  * Description: The attributes of a dog.
  */
